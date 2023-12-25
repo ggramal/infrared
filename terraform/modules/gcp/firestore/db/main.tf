20169d2b667c9b48568c10211a7e030dabf719de
Gramovich Alexander <gramovich.alex@outlook.com>
2023-12-25 18:33:13 +0300

    Add the ability to set app engine instead of firestore

--- terraform/modules/gcp/firestore/db/main.tf
+++ terraform/modules/gcp/firestore/db/main.tf
@@ -1,4 +1,5 @@
 resource "google_firestore_database" "this" {
+  count                       = var.app_engine_integration_mode == "DISABLED" ? 1 : 0
   name                        = var.name
   location_id                 = var.location_id
   type                        = var.type
@@ -7,3 +8,14 @@ resource "google_firestore_database" "this" {
   delete_protection_state     = var.delete_protection_state
   deletion_policy             = var.deletion_policy
 }
+
+
+resource "google_app_engine_application" "this" {
+  count         = var.app_engine_integration_mode == "ENABLED" ? 1 : 0
+  project       = data.google_project.project.project_id
+  location_id   = var.location_id
+  database_type = "CLOUD_DATASTORE_COMPATIBILITY"
+}
+
+
+data "google_project" "project" {}
